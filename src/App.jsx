e79bc9223d2a70e572f9fda309c049827653d203
19guptayash <yash.gupta1@daffodilsw.com>
2024-02-12 17:24:53 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <div className="text-3xl font-bold underline" >
+        helloworld
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/redux/UserSlice.js
@@ -0,0 +1,17 @@
+
+const initialState = {
+    user:{
+
+    },
+    favourite:[],
+    watchList:[]
+}
+export const userSlice = {
+    name:"user",
+    initialState,
+    reducer:{
+
+    }
+}
+
+export default userSlice.reducer();
